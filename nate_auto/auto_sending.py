0ace722472bd2eeb316e5380580aebb945634f72
unknown <lovingisluck@naver.com>
2020-12-26 21:19:52 +0900

    find nateON process id automatically

--- nate_auto/auto_sending.py
+++ nate_auto/auto_sending.py
@@ -7,15 +7,25 @@ class send_auto():
         self.file_name = os.path.basename(path)
         self.dir_path  = os.path.dirname (path)
 
-        self.pid       =  4664
+        self.pid       =  self.get_nate_pid()
         self.name      =  "이미향"
         self.chat_name =  self.name + "님 메시지"
+        self.chat_name = "내게 쓰기" # for developing
         self.msg_area  =  "RICHEDIT50W2"
         self.file_exp_n = "열기"
 
         self.app       = Application(backend='win32').connect(process=self.pid)
         self.chat_dial = self.app[self.chat_name]
 
+    def get_nate_pid(self):
+        procs = pywinauto.findwindows.find_elements()
+        for proc in procs :
+            print(type(proc))
+            print(proc.name)
+            if 'nate' in proc.name.lower() :
+                nate_pid = proc.process_id
+                break
+        return nate_pid
     def start(self):
         self.chat_dial.type_keys("^t")
         
